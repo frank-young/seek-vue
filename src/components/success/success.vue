5957caa2d15d8ba09251e20d7d148bbe5cc4edea
frank <yangjunalns@qq.com>
2017-01-09 02:17:47 +0800

    下单成功页面

---
+++ src/components/success/success.vue
@@ -0,0 +1,130 @@
+<template>
+	<div class="success">
+		<div class="success-close">
+			<i class="icon-close" @click="close"></i>
+			<span class="close-txt">{{text}}</span>
+		</div>
+		<div class="success-content">
+			<div class="success-icon">
+				<i class="icon-order"></i>
+			</div>
+			<div class="success-text">
+				<h3>下单成功</h3>
+				<p>我们正尽快为您准备</p>
+			</div>
+		</div>
+		<div class="success-btn-group">
+			<button class="btn" type="button" @click="close">返回首页</button>
+			<button class="btn" type="button" @click="order">查看订单</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import VueRouter from 'vue-router'
+
+const router = new VueRouter({})
+
+export default {
+	props: {
+	},
+	data() {
+		return {
+			text: '下单成功'
+		}
+	},
+	methods: {
+		close() {
+			router.push('goods')
+		},
+		order() {
+			router.push('goods')
+		}
+	},
+	components: {
+	}
+}
+</script>
+
+<style lang="less">
+	.success {
+		position: fixed;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 60;
+		background-color: #fff;
+		.success-close{
+			width: 100%;
+			height: 48px;
+			line-height: 48px;
+			background-color: #3290e8;
+			i{
+				display: inline-block;
+				font-size: 24px;
+				padding: 12px 10px 12px 20px;
+				margin-right: 10px;
+				color: #fff;
+			}
+			.close-txt {
+				display: inline-block;
+				vertical-align: top;
+				font-size: 16px;
+				color: #fff
+			}	
+		}
+		
+		.success-content{
+			display: flex;
+			margin-top: 20px;
+		}
+		.success-icon {
+			flex: 2;
+			text-align: right;
+			margin-right: 20px;
+			i {
+				font-size: 60px;
+				color: #3dba84;
+			}
+		}
+		.success-text {
+			flex: 3;
+			h3 {
+				font-size: 18px;
+				margin-top: 10px;
+				margin-bottom: 10px;
+				color: #333;
+			}
+			p{
+				font-size: 14px;
+				color: #888;
+			}
+		}
+		.success-btn-group{
+			display: flex;
+			padding: 0 20px;
+			margin-top: 30px;
+			.btn{
+				flex: 1;
+				margin: 5px;
+				padding: 8px;
+				font-size: 15px;
+				border-radius: 2px;
+				border: 1px solid #ddd;
+				background-color: #fff;
+				color: #555;
+				outline: none;
+			}
+		}
+	}
+</style>
+
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import ratings from 'components/ratings/ratings'
 import seller from 'components/seller/seller'
 import pay from 'components/pay/pay'
 import watingpay from 'components/watingpay/watingpay'
+import success from 'components/success/success'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
@@ -16,7 +17,8 @@ const routes = [
   {path: '/ratings', component: ratings, name: '评价'},
   {path: '/seller', component: seller, name: '商家'},
   {path: '/pay', component: pay, name: '在线支付'},
-  {path: '/watingpay', component: watingpay, name: '等待支付'}
+  {path: '/watingpay', component: watingpay, name: '等待支付'},
+  {path: '/success', component: success, name: '下单成功'}
 ]
 
 const router = new VueRouter({
